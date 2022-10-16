eb4a1051d3b24a9fed4d35bdfbb83a19c1c0dd5f
Jared Storm <jaredrstorm@gmail.com>
2022-10-16 11:38:45 -0400

    Create basic nav menu

---
+++ components/Navbar.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
+
+const MENU = [
+  { text: "Home", href: "/" },
+  { text: "Practice", href: "/practice" },
+  { text: "Settings", href: "/settings" },
+];
+
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <a>
+            <h1 className="logo">L2Type</h1>
+          </a>
+        </Link>
+        {/* <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div> */}
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
