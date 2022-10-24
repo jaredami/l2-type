76a912786bd883d98f39085aae1455fd89774e25
Jared Storm <jaredrstorm@gmail.com>
2022-10-23 21:17:55 -0400

    Add spacing to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
-import Nav from "./Nav/Nav";
+import Nav from "../Nav/Nav";
+import styles from "./Layout.module.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Nav />
-      <main>{children}</main>
+      <main className={styles.layout}>{children}</main>
     </>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
+import Layout from "../components/Layout/Layout";
 import "../styles/globals.css";
 import "../styles/design_tokens.css";
 
