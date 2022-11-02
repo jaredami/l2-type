4b96925c0e06b833df218dc356c2ff88440d0b67
Jared Storm <jaredrstorm@gmail.com>
2022-11-01 22:36:41 -0400

    Extract functions from component to fix useEffect deps

--- pages/practice.tsx
+++ pages/practice.tsx
@@ -5,10 +5,42 @@ import Keyboard from "../components/Keyboard/Keyboard";
 const SAMPLE_LENGTH = 5;
 const includeCapitals = false;
 
+function getRandomWords() {
+  const wordsArr: string[] = [];
+  for (let i = 0; i < SAMPLE_LENGTH; i++) {
+    wordsArr.push(getRandomWord(getRandomNumber(10, 2)));
+  }
+  return wordsArr;
+}
+
+function getRandomWord(wordLength = 10) {
+  let consonants = "bcdfghjlmnpqrstv".split("");
+  let vowels = "aeiou".split("");
+  let word = "";
+  let length = parseInt(wordLength.toString(), 10);
+
+  for (let i = 0; i < length / 2; i++) {
+    const randConsonant = consonants[getRandomNumber(consonants.length)];
+    const randVowel = vowels[getRandomNumber(vowels.length)];
+
+    if (includeCapitals) {
+      word += i === 0 ? randConsonant.toUpperCase() : randConsonant;
+    } else {
+      word += i === 0 ? randConsonant : randConsonant;
+    }
+    word += i * 2 < length - 1 ? randVowel : "";
+  }
+
+  return word;
+}
+
+function getRandomNumber(upperLimit: number, lowerLimit = 0) {
+  return Math.floor(Math.random() * upperLimit) + lowerLimit;
+}
+
 export default function Practice() {
   const [sample, setSample] = useState<string[]>([]);
 
-  // TODO fix dependency array
   useEffect(() => {
     function getSample() {
       return getRandomWords().join(" ").replaceAll(" ", "_").split("");
@@ -16,39 +48,6 @@ export default function Practice() {
 
     setSample(getSample());
   }, []);
-
-  function getRandomWords() {
-    const wordsArr: string[] = [];
-    for (let i = 0; i < SAMPLE_LENGTH; i++) {
-      wordsArr.push(getRandomWord(getRandomNumber(10, 2)));
-    }
-    return wordsArr;
-  }
-
-  function getRandomWord(wordLength = 10) {
-    let consonants = "bcdfghjlmnpqrstv".split("");
-    let vowels = "aeiou".split("");
-    let word = "";
-    let length = parseInt(wordLength.toString(), 10);
-
-    for (let i = 0; i < length / 2; i++) {
-      const randConsonant = consonants[getRandomNumber(consonants.length)];
-      const randVowel = vowels[getRandomNumber(vowels.length)];
-
-      if (includeCapitals) {
-        word += i === 0 ? randConsonant.toUpperCase() : randConsonant;
-      } else {
-        word += i === 0 ? randConsonant : randConsonant;
-      }
-      word += i * 2 < length - 1 ? randVowel : "";
-    }
-
-    return word;
-  }
-
-  function getRandomNumber(upperLimit: number, lowerLimit = 0) {
-    return Math.floor(Math.random() * upperLimit) + lowerLimit;
-  }
 
   return (
     <>
