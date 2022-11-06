70c6c48a068a142f9db122161184cea72f47b58b
Jared Storm <jaredrstorm@gmail.com>
2022-11-06 10:21:07 -0500

    Convert MENU const to object

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import React, { useState } from "react";
-import NavItem from "./NavItem/NavItem";
+import { useState } from "react";
 import styles from "./Nav.module.css";
 
-const MENU = [
-  { text: "Home", href: "/" },
-  { text: "Practice", href: "/practice" },
-  { text: "Settings", href: "/settings" },
-];
+const MENU = {
+  home: { text: "L2TYPE", href: "/" },
+  practice: { text: "Practice", href: "/practice" },
+  settings: { text: "Settings", href: "/settings" },
+};
 
 const Nav = () => {
   const [activeIdx, setActiveIdx] = useState(-1);
@@ -15,10 +14,10 @@ const Nav = () => {
   return (
     <header>
       <nav className={styles.nav}>
-        <Link href={"/"}>
+        <Link href={MENU.home.href}>
           <li className={[styles.logo, styles.navLink].join(" ")}>
             <span className={[styles.linkText, styles.logoText].join(" ")}>
-              L2 Type
+              {MENU.home.text}
             </span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -37,11 +36,8 @@ const Nav = () => {
           </li>
         </Link>
         <ul className={styles.navList}>
-          {/* {MENU.map((item, idx) => (
-            <NavItem active={activeIdx === idx} {...item} key={item.text} />
-          ))} */}
           <li className={styles.navItem}>
-            <Link href={MENU[1].href}>
+            <Link href={MENU.practice.href}>
               <a href="#" className={styles.navLink}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -57,10 +53,10 @@ const Nav = () => {
                     d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
                   />
                 </svg>
-                <span className={styles.linkText}>{MENU[1].text}</span>
+                <span className={styles.linkText}>{MENU.practice.text}</span>
               </a>
             </Link>
-            <Link href={MENU[2].href}>
+            <Link href={MENU.settings.href}>
               <a href="#" className={styles.navLink}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -76,7 +72,7 @@ const Nav = () => {
                     d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
                   />
                 </svg>
-                <span className={styles.linkText}>{MENU[2].text}</span>
+                <span className={styles.linkText}>{MENU.settings.text}</span>
               </a>
             </Link>
           </li>
