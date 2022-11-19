b3d5f2991a52a712ff90e4169461b6069d998c90
Jared Storm <jaredrstorm@gmail.com>
2022-11-19 17:06:22 -0500

    Create stats page

---
+++ pages/stats.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Stats() {
+  return (
+    <>
+      <Head>
+        <title>Stats - L2Type</title>
+      </Head>
+      <h1>Stats</h1>
+    </>
+  );
+}
