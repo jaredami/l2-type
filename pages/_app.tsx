f12eb11207a41cd2618bc4f178b13f6ee9cf5611
Jared Storm <jaredrstorm@gmail.com>
2022-11-19 17:43:39 -0500

    Wrap app in StatsContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,15 @@ import "../styles/globals.css";
 import "../styles/design_tokens.css";
 import "../styles/keyboard.css";
 import { SettingsProvider } from "../contexts/SettingsContext";
+import { StatsProvider } from "../contexts/StatsContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <SettingsProvider>
-        <Component {...pageProps} />
+        <StatsProvider>
+          <Component {...pageProps} />
+        </StatsProvider>
       </SettingsProvider>
     </Layout>
   );
