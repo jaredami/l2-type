d1da55db74bbee83b888b81d45521a6993fd4801
Jared Storm <jaredrstorm@gmail.com>
2022-11-20 11:11:19 -0500

    Add actual stat labels

--- components/StatsPanel/StatsPanel.tsx
+++ components/StatsPanel/StatsPanel.tsx
@@ -5,15 +5,19 @@ export default function StatsPanel() {
   return (
     <div className={styles.statsContainer}>
       <div className={styles.statContainer}>
-        <p>Stat</p>
+        <p className={styles.statLabel}>Average Speed</p>
         <p>33</p>
       </div>
       <div className={styles.statContainer}>
-        <p>Stat</p>
+        <p className={styles.statLabel}>Average Accuracy</p>
         <p>33</p>
       </div>
       <div className={styles.statContainer}>
-        <p>Stat</p>
+        <p className={styles.statLabel}>Total Lessons</p>
+        <p>33</p>
+      </div>
+      <div className={styles.statContainer}>
+        <p className={styles.statLabel}>Top Speed</p>
         <p>33</p>
       </div>
     </div>
