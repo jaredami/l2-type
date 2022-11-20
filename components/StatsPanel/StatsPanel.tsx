e065d5eb7ecc43d45995b81bde0cd7827f0dedf5
Jared Storm <jaredrstorm@gmail.com>
2022-11-20 11:13:54 -0500

    Display average speed

--- components/StatsPanel/StatsPanel.tsx
+++ components/StatsPanel/StatsPanel.tsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StatsContext } from "../../contexts/StatsContext";
 import styles from "./StatsPanel.module.css";
 
 export default function StatsPanel() {
+  const stats = useContext(StatsContext);
   return (
     <div className={styles.statsContainer}>
       <div className={styles.statContainer}>
         <p className={styles.statLabel}>Average Speed</p>
-        <p>33</p>
+        <p>{stats?.averageWpm} wpm</p>
       </div>
       <div className={styles.statContainer}>
         <p className={styles.statLabel}>Average Accuracy</p>
-        <p>33</p>
+        <p>--</p>
       </div>
       <div className={styles.statContainer}>
         <p className={styles.statLabel}>Total Lessons</p>
-        <p>33</p>
+        <p>--</p>
       </div>
       <div className={styles.statContainer}>
         <p className={styles.statLabel}>Top Speed</p>
-        <p>33</p>
+        <p>--</p>
       </div>
     </div>
   );
