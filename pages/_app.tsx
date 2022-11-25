609efaf36a6316c71c793ddafebde34d83d758bf
Jared Storm <jaredrstorm@gmail.com>
2022-11-25 15:31:50 -0500

    Wrap app in SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,29 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout/Layout";
-import "../styles/globals.css";
-import "../styles/design_tokens.css";
-import "../styles/keyboard.css";
 import { SettingsProvider } from "../contexts/SettingsContext";
 import { StatsProvider } from "../contexts/StatsContext";
+import "../styles/design_tokens.css";
+import "../styles/globals.css";
+import "../styles/keyboard.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
-    <Layout>
-      <SettingsProvider>
-        <StatsProvider>
-          <Component {...pageProps} />
-        </StatsProvider>
-      </SettingsProvider>
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <SettingsProvider>
+          <StatsProvider>
+            <Component {...pageProps} />
+          </StatsProvider>
+        </SettingsProvider>
+      </Layout>
+    </SessionProvider>
   );
 }
 
