716fe48ec9fb089285a2cf49ff9e2390f0f51399
Jared Storm <jaredrstorm@gmail.com>
2022-11-25 21:02:00 -0500

    Add Sign In button

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,29 @@
 import { ReactNode } from "react";
 import Nav from "../Nav/Nav";
 import styles from "./Layout.module.css";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const { data, status } = useSession();
+
+  const handleSignIn = async () => {
+    await signIn("github", {
+      callbackUrl: "http://localhost:3000/practice",
+    });
+  };
+
   return (
     <>
       <Nav />
+      <button
+        style={{
+          right: 0,
+          position: "absolute",
+        }}
+        onClick={handleSignIn}
+      >
+        Sign In
+      </button>
       <main className={styles.layout}>{children}</main>
     </>
   );
