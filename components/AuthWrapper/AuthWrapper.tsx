18abd5223d0fdada40e2eecc2b58f296ebaa9c6a
Jared Storm <jaredrstorm@gmail.com>
2022-11-26 17:06:32 -0500

    Add ProtectedRoute component

--- components/AuthWrapper/AuthWrapper.tsx
+++ components/AuthWrapper/AuthWrapper.tsx
@@ -1,9 +1,9 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
-// import ProtectedRoute from "./ProtectedRoute";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
-const authRoutes = ["/dashboard"];
+const authRoutes = ["/practice", "/settings", "/stats"];
 
 const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
   const session = useSession();
@@ -14,10 +14,11 @@ const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
-      {authRoutes.includes(router.pathname)
-        ? // <ProtectedRoute>{children}</ProtectedRoute>
-          { children }
-        : children}
+      {authRoutes.includes(router.pathname) ? (
+        <ProtectedRoute>{children}</ProtectedRoute>
+      ) : (
+        children
+      )}
     </>
   );
 };
