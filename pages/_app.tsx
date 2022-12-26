f6e6e45c83e477ec379896863cbd18cb89f4a371
Jared Storm <jaredrstorm@gmail.com>
2022-12-25 19:39:11 -0500

    Delete SettingsContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import AuthWrapper from "../components/AuthWrapper/AuthWrapper";
 import Layout from "../components/Layout/Layout";
-import { SettingsProvider } from "../contexts/SettingsContext";
 import { StatsProvider } from "../contexts/StatsContext";
 import "../styles/design_tokens.css";
 import "../styles/globals.css";
@@ -19,11 +18,9 @@ function MyApp({
     <SessionProvider session={pageProps.session}>
       <AuthWrapper>
         <Layout>
-          <SettingsProvider>
-            <StatsProvider>
-              <Component {...pageProps} />
-            </StatsProvider>
-          </SettingsProvider>
+          <StatsProvider>
+            <Component {...pageProps} />
+          </StatsProvider>
         </Layout>
       </AuthWrapper>
     </SessionProvider>
